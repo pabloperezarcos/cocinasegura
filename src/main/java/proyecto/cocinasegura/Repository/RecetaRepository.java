f4f8f65a8534263c1e540f17d6eb40ed1a9e9b07
pabloperezarcos <pabloperezarcos@gmail.com>
2024-11-02 21:41:08 -0300

    Update RecetaRepository.java

--- src/main/java/proyecto/cocinasegura/Repository/RecetaRepository.java
+++ src/main/java/proyecto/cocinasegura/Repository/RecetaRepository.java
@@ -1,5 +1,20 @@
 package proyecto.cocinasegura.Repository;
 
-public class RecetaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import proyecto.cocinasegura.Model.Receta;
+import java.util.List;
 
+public interface RecetaRepository extends JpaRepository<Receta, Long>, JpaSpecificationExecutor<Receta> {
+
+    // Métodos de consultas
+    List<Receta> findByTituloContainingIgnoreCase(String titulo);
+
+    List<Receta> findByTipoDeCocina(String tipoDeCocina);
+
+    List<Receta> findByIngredientesContainingIgnoreCase(String ingredientes);
+
+    List<Receta> findByPaisDeOrigen(String paisDeOrigen);
+
+    List<Receta> findByDificultad(String dificultad);
 }
