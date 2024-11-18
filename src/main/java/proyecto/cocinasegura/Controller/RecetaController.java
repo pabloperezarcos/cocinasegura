b41ecc40681f2392f0ae8820c523c35ed3d74f80
Pablo Perez Arcos <pabloperezarcos@gmail.com>
2024-11-18 10:32:25 -0300

    Update RecetaController.java

--- src/main/java/proyecto/cocinasegura/Controller/RecetaController.java
+++ src/main/java/proyecto/cocinasegura/Controller/RecetaController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/recetas")
+@RequestMapping("/api/recetas2")
 public class RecetaController {
 
     @Autowired
