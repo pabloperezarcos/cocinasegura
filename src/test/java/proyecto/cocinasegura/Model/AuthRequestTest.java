5666e63eb644aefd2cbf4fbdf64b60fb74db9fcc
Pablo Perez Arcos <pabloperezarcos@gmail.com>
2024-12-10 11:09:50 -0300

    Update AuthRequestTest.java

--- src/test/java/proyecto/cocinasegura/Model/AuthRequestTest.java
+++ src/test/java/proyecto/cocinasegura/Model/AuthRequestTest.java
@@ -1,9 +1,11 @@
 package proyecto.cocinasegura.Model;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SpringBootTest
 public class AuthRequestTest {
 
     @Test
